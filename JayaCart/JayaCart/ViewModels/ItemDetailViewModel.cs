4a18e3aaff26167876a853f3a87889b4a4fa0763
Rubal Walia <walia.rubal@gmail.com>
2020-02-11 22:25:28 +0530

    Add project files.

---
+++ JayaCart/JayaCart/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using JayaCart.Models;
+
+namespace JayaCart.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
