90f84ffe5c4d1a84a60a285dd652c3f01de4fd30
Rubal Walia <walia.rubal@gmail.com>
2020-02-12 12:29:08 +0530

    Added side menu.

---
+++ src/JayaCart/View/MainViewMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JayaCart.View
+{
+
+    public class MainViewMenuItem
+    {
+        public MainViewMenuItem()
+        {
+            TargetType = typeof(ItemsView);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
