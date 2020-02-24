837300d6c36df4062aae3488b36b19c33515cde3
Rubal Walia <walia.rubal@gmail.com>
2020-02-24 12:29:46 +0530

    Added initial code for server API.

---
+++ src-server/functions/src/index.ts
@@ -0,0 +1,69 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as firebaseHelper from 'firebase-functions-helper';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+import { UserAccount } from './models/user-account';
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+
+const main = express();
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+export const webApi = functions.https.onRequest(main);
+
+const USER_ACCOUNTS = 'UserAccounts';
+
+const db = admin.firestore();
+
+app.post(`/${USER_ACCOUNTS}`, async (request, response) => {
+    try {
+        let account: UserAccount = {
+            PhoneNumber: request.body['PhoneNumber'],
+            FirstName: request.body['FirstName'],
+            LastName: request.body['LastName'],
+            AddressLine1: request.body['AddressLine1'],
+            AddressLine2: request.body['AddressLine2'],
+            City: request.body['City'],
+            Password: request.body['Password'],
+            Image: request.body['Image'],
+            Balance: 0
+        };
+
+        await firebaseHelper.firestore.createDocumentWithID(db, USER_ACCOUNTS, account.PhoneNumber, account);
+        response.status(201).send(`User account with phone number ${account.PhoneNumber} created.`);
+    } catch (ex) {
+        response.send(400).send(`Failed to create user account.`);
+    }
+});
+
+app.patch(`/${USER_ACCOUNTS}/:PhoneNumber`, async (request, response) => {
+    try {
+        await firebaseHelper.firestore.updateDocument(db, USER_ACCOUNTS, request.params.PhoneNumber, request.body);
+        response.send(204).send(`User account with phone number ${request.params.PhoneNumber} updated.`);
+    } catch (ex) {
+        response.send(400).send(`Failed to update user account with phone number ${request.params.PhoneNumber}.`);
+    }
+});
+
+app.get(`/${USER_ACCOUNTS}/:PhoneNumber`, (request, response) => {
+    firebaseHelper.firestore.getDocument(db, USER_ACCOUNTS, request.params.PhoneNumber)
+        .then(record => response.status(200).send(record))
+        .catch(ex => response.status(400).send(`Failed to get user account for phone number ${request.params.PhoneNumber}: ${ex}`));
+});
+
+app.get(`/${USER_ACCOUNTS}`, (request, response) => {
+    firebaseHelper.firestore.backup(db, USER_ACCOUNTS)
+        .then(records => response.status(200).send(records))
+        .catch(ex => response.status(400).send(`Failed to get user accounts: ${ex}`));
+});
+
+app.delete(`/${USER_ACCOUNTS}/:PhoneNumber`, async (request, response) => {
+    const deletedRecord = await firebaseHelper.firestore.deleteDocument(db, USER_ACCOUNTS, request.params.PhoneNumber);
+    response.status(204).send(`User account deleted: ${deletedRecord}`);
+});
---
+++ src-server/functions/src/models/user-account.ts
@@ -0,0 +1,11 @@
+export class UserAccount {
+    PhoneNumber: string;
+    FirstName: string;
+    LastName: string;
+    AddressLine1: string;
+    AddressLine2: string;
+    City: string;
+    Password: string;
+    Image: string;
+    Balance: number;
+}
