6574ac95f0812911bf474630fec2691e7357f98b
Rubal Walia <walia.rubal@gmail.com>
2020-03-03 14:20:06 +0530

    Improved code quality for user account service.

--- src-web/functions/src/services/user-account.service.ts
+++ src-web/functions/src/services/user-account.service.ts
@@ -11,61 +11,67 @@ export class UserAccountService extends BaseService {
         super(db, app);
     }
 
-    RegisterMethods(): void {
-        this.Application.post(`/${this.USER_ACCOUNTS}`, async (request, response) => {
-            try {
-                let account: UserAccount = {
-                    PhoneNumber: request.body['PhoneNumber'],
-                    FirstName: request.body['FirstName'],
-                    LastName: request.body['LastName'],
-                    AddressLine1: request.body['AddressLine1'],
-                    AddressLine2: request.body['AddressLine2'],
-                    City: request.body['City'],
-                    Password: request.body['Password'],
-                    Image: request.body['Image'],
-                    Balance: 0,
-                    IsActive: false,
-                    IsAdmin: false
-                };
+    private async CreateAccount(request, response) {
+        try {
+            let account: UserAccount = {
+                PhoneNumber: request.body['PhoneNumber'],
+                FirstName: request.body['FirstName'],
+                LastName: request.body['LastName'],
+                AddressLine1: request.body['AddressLine1'],
+                AddressLine2: request.body['AddressLine2'],
+                City: request.body['City'],
+                Password: request.body['Password'],
+                Image: request.body['Image'],
+                Balance: 0,
+                IsActive: false,
+                IsAdmin: false
+            };
 
-                let isCreated = await firebaseHelper.firestore.createDocumentWithID(this.Database, this.USER_ACCOUNTS, account.PhoneNumber, account);
-                if (isCreated)
-                    response.status(201).json(account);
-                else {
-                    console.log(`Failed to create user account: ${account}`);
-                    response.status(400).send(`Failed to create user account.`);
-                }
-
-            } catch (ex) {
-                console.log(`Failed to create user account. ${ex}`);
+            let isCreated = await firebaseHelper.firestore.createDocumentWithID(this.Database, this.USER_ACCOUNTS, account.PhoneNumber, account);
+            if (isCreated)
+                response.status(201).json(account);
+            else {
+                console.log(`Failed to create user account: ${account}`);
                 response.status(400).send(`Failed to create user account.`);
             }
-        });
 
-        this.Application.patch(`/${this.USER_ACCOUNTS}/:PhoneNumber`, async (request, response) => {
-            try {
-                await firebaseHelper.firestore.updateDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber, request.body);
-                response.status(204).json(request.body);
-            } catch (ex) {
-                response.status(400).send(`Failed to update user account with phone number ${request.params.PhoneNumber}.`);
-            }
-        });
+        } catch (ex) {
+            console.log(`Failed to create user account. ${ex}`);
+            response.status(400).send(`Failed to create user account.`);
+        }
+    }
 
-        this.Application.get(`/${this.USER_ACCOUNTS}/:PhoneNumber`, (request, response) => {
-            firebaseHelper.firestore.getDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber)
-                .then(record => response.status(200).json(record))
-                .catch(ex => response.status(400).send(`Failed to get user account for phone number ${request.params.PhoneNumber}: ${ex}`));
-        });
+    private async UpdateAccount(request, response) {
+        try {
+            await firebaseHelper.firestore.updateDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber, request.body);
+            response.status(204).json(request.body);
+        } catch (ex) {
+            response.status(400).send(`Failed to update user account with phone number ${request.params.PhoneNumber}.`);
+        }
+    }
 
-        this.Application.get(`/${this.USER_ACCOUNTS}`, (request, response) => {
-            firebaseHelper.firestore.backup(this.Database, this.USER_ACCOUNTS)
-                .then(records => response.status(200).json(records))
-                .catch(ex => response.status(400).send(`Failed to get user accounts: ${ex}`));
-        });
+    private GetAccountByPhoneNumber(request, response) {
+        firebaseHelper.firestore.getDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber)
+            .then(record => response.status(200).json(record))
+            .catch(ex => response.status(400).send(`Failed to get user account for phone number ${request.params.PhoneNumber}: ${ex}`));
+    }
 
-        this.Application.delete(`/${this.USER_ACCOUNTS}/:PhoneNumber`, async (request, response) => {
-            const deletedRecord = await firebaseHelper.firestore.deleteDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber);
-            response.status(204).json(deletedRecord);
-        });
+    private GetAccount(request, response) {
+        firebaseHelper.firestore.backup(this.Database, this.USER_ACCOUNTS)
+            .then(records => response.status(200).json(records))
+            .catch(ex => response.status(400).send(`Failed to get user accounts: ${ex}`));
+    }
+
+    private async DeleteAccount(request, response) {
+        const deletedRecord = await firebaseHelper.firestore.deleteDocument(this.Database, this.USER_ACCOUNTS, request.params.PhoneNumber);
+        response.status(204).json(deletedRecord);
+    }
+
+    RegisterMethods(): void {
+        this.Application.post(`/${this.USER_ACCOUNTS}`, this.CreateAccount);
+        this.Application.patch(`/${this.USER_ACCOUNTS}/:PhoneNumber`, this.UpdateAccount);
+        this.Application.get(`/${this.USER_ACCOUNTS}/:PhoneNumber`, this.GetAccountByPhoneNumber);
+        this.Application.get(`/${this.USER_ACCOUNTS}`, this.GetAccount);
+        this.Application.delete(`/${this.USER_ACCOUNTS}/:PhoneNumber`, this.DeleteAccount);
     }
 }
