c720d5fdb1868ff343475da8d09201fde097eb62
Rubal Walia <walia.rubal@gmail.com>
2020-03-08 23:00:37 +0530

    Staging changes.

--- src-web/client/src/app/app.module.ts
+++ src-web/client/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 import { UserAccountModule } from './user-account/user-account.module';
+import { AccountsComponent } from './user-account/accounts/accounts.component';
+
+const routes: Routes = [
+  { path: 'UserAccounts', component: AccountsComponent },
+  { path: '', redirectTo: '/Dashboard', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     HttpClientModule,
+    RouterModule.forRoot(routes, { enableTracing: true }),
+    NgbModule,
     UserAccountModule
   ],
   providers: [],
